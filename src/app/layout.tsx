57a293f0a2b73c8cde06e45d0374d738b20954de
teyim <teyimasobo@gmail.com>
2025-06-26 22:00:22 +0100

    modify analytics intergration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { PostHogProvider } from "./providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,11 +30,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased font-mono`}
       >
+        <PostHogProvider>
         <Navbar />
         <main className="min-h-[80vh] flex flex-col">
           {children}
         </main>
         <Footer />
+        </PostHogProvider>
       </body>
     </html>
   );
